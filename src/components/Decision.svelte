616279ccc48f47adc7bf6b8e551133da98e3848e
russellgoldenberg <russellgoldenberg@gmail.com>
2021-01-12 15:38:27 -0500

    custom radio

--- src/components/Decision.svelte
+++ src/components/Decision.svelte
@@ -20,3 +20,28 @@
     </fieldset>
   </div>
 {/if}
+
+<style>
+  label {
+    user-select: none;
+    cursor: pointer;
+  }
+
+  input {
+    position: absolute;
+    clip: rect(1px, 1px, 1px, 1px);
+    padding: 0;
+    border: 0;
+    height: 1px;
+    width: 1px;
+    overflow: hidden;
+  }
+
+  input[type="radio"]:checked + label {
+    background: yellow;
+  }
+
+  input[type="radio"]:focus + label {
+    box-shadow: 0 0 0 2px var(--focus);
+  }
+</style>
